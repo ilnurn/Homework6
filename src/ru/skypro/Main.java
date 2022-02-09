fe43b3c8f765ad3e25d81b9218bfe3dc4350cda8
Ilnur <nabiulli@mail.ru>
2022-02-09 23:01:37 +0500

    Домашнее задание по второй части темы массивы

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,42 @@
 package ru.skypro;
 
-public class Main {
+import java.util.Arrays;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+
+        int[] daySpending = new int[30];
+
+        int monthSpending = 0;
+        int maximumDaySpending = 100_000;
+        int minimumDaySpending = 200_000;
+
+        for (int i = 0; i < 30; i++) {
+            daySpending[i] = (int) ((100 * Math.random() + 100) * 1000);
+            System.out.print(daySpending[i] + " ");
+            monthSpending = monthSpending + daySpending[i];
+            if (daySpending[i] >= maximumDaySpending) {
+                maximumDaySpending = daySpending[i];
+            }
+            if (daySpending[i] <= minimumDaySpending) {
+                minimumDaySpending = daySpending[i];
+            }
+        }
+        // Задание 1
+        System.out.println();
+        System.out.println( "Сумма трат за месяц составила " + monthSpending + " рублей");
+        // Задание 2
+        System.out.println("Минимальная сумма трат за день составила " + minimumDaySpending + " рублей");
+        System.out.println("Максимальная сумма трат за день составила " + maximumDaySpending + " рублей");
+        // Задание 3
+        float averageMonthSpending = (float) monthSpending / 30;
+        System.out.println("Средняя сумма трат за месяц составила " + averageMonthSpending + " рублей");
+
+        // Задание 4
+        char[] reversFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reversFullName.length - 1; i >= 0; i--) {
+            System.out.print(reversFullName[i]);
+        }
+
     }
 }
