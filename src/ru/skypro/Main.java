0073e3cf067f86e6fc182944e86ca0762636bd01
Ilnur <nabiulli@mail.ru>
2022-02-13 13:46:55 +0500

    Добавил решения заданий повышенной сложности номер 5, 6, 7, , 8, 9

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -37,6 +37,73 @@ public class Main {
         for (int i = reversFullName.length - 1; i >= 0; i--) {
             System.out.print(reversFullName[i]);
         }
+        System.out.println();
 
+        // Задание 5
+        int a = 1;
+        int[][] matrix = {{a, 0, a}, {0, a, 0}, {a, 0, a}};
+        for (int[] row : matrix) {
+            for (int column : row) {
+                if (column == 0) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print(column + " ");
+                }
+            }
+            System.out.println();
+        }
+
+        // Задание 6
+        int[] arr = {5, 4, 3, 2, 1};
+        System.out.println(Arrays.toString(arr));
+
+        int[] arr2 = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr2[i] = arr[(arr.length - 1) - i];
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr2[i];
+        }
+        System.out.println(Arrays.toString(arr));
+
+        // Задание 7
+        arr = new int[]{5, 4, 3, 2, 1};
+
+        System.out.println(Arrays.toString(arr));
+        for (int index = 0; index < arr.length / 2; index++) {
+            arr[index] = arr[index] + arr[arr.length - 1 - index];
+            arr[arr.length - 1 - index] = arr[index] - arr[(arr.length - 1) - index];
+            arr[index] = arr[index] - arr[arr.length - 1 - index];
+        }
+        System.out.println(Arrays.toString(arr));
+
+        // Задание 8
+        int[] array = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
+        Arrays.sort (array);
+        int negative = 0;
+        int positive = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                negative = array[i];
+            } else {
+                if (array[i] > 0) {
+                    positive = array[i];
+                }
+                if (negative + positive == -2) {
+                    System.out.println("Задание 8: числа " + negative + " и " + positive);
+                }
+            }
+        }
+
+        // Задание 9
+        int[] numbers = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
+        for (int i = 0; i < numbers.length; i++) {
+            for (int i1 = i + 1; i1 < numbers.length; i1++) {
+                if (numbers[i] + numbers[i1] == -2) {
+                    int sum = numbers[i] + numbers[i1];
+                    System.out.println("Сумма чисел " + numbers[i] +" и " + numbers[i1] + " равна " + sum);
+                }
+            }
+        }
     }
 }
